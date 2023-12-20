8f9c4bdc6f513eb258ad2a6d3c946676a735db0f
ingri <mokymui25@gmail.com>
2023-12-20 19:45:42 +0200

    add async crawling with threads to main.py

--- ingrida_data_kup/main.py
+++ ingrida_data_kup/main.py
@@ -1,36 +1,25 @@
-from concurrent.futures import ThreadPoolExecutor, wait
-from queue import Queue
 from lxml.etree import HTML
 from requests import get
-visited_pages = set()
-visited_articles = set()
-pages_queue = Queue()
-articles_queue = Queue()
-def query_to_url(query: str) -> str:
-return f"https://www.lrytas.lt/search?q={query}"
-def process_page(page_url: str):
-try:
-response = get(page_url)
-tree = HTML(response.text)
-for page_link in tree.xpath(
-"//a[contains(@class, 'LPagination__button')]/@href"
-):
-page_url = "https://www.lrytas.lt" + page_link
-if page_url not in visited_pages:
-pages_queue.put(page_url)
-visited_pages.add(page_url)
-for article_link in tree.xpath("//a[@class='LPostContent__anchor']/@href"):
-article_url = "https://www.lrytas.lt" + article_link
-if article_url not in visited_articles:
-visited_articles.add(article_url)
-articles_queue.put(article_url)
-except Exception:
-print("Error retrieving url")
-if __name__ == "__main__":
-process_page(query_to_url("rytas"))
-with ThreadPoolExecutor(10) as pool:
-futures = [pool.submit(process_page, query_to_url("rytas"))]
-while wait(futures):
-while not pages_queue.empty():
-page = pages_queue.get()
-futures.append(pool.submit(process_page, page))
+import csv
+
+data= get("https://www.gintarine.lt").text
+data= HTML(data)
+medical_name= data.xpath("//a/img[contains(@class, 'category-grid__title')]/@alt")
+
+medical_image=data.xpath("//a/img[contains(@class, 'img-responsive')]/@src")
+medical_image_url=[]
+for link in data.xpath("//a/img[contains(@class, 'img-responsive')]/@src"):
+    medical_image_url.append("https://www.gintarine.lt" + link)
+
+medical_page=data.xpath("//a[contains(@class,'vehicle-item fp-cta-gtm')]/@href")
+medical_page_url=[]
+for link in data.xpath("//a[contains(@class,'vehicle-item fp-cta-gtm')]/@href"):
+   medical_page_url.append("https://www.gintarine.lt" + link)
+
+with open("Medical.csv", "w") as file_writer:
+    fieldnames=['Pavadinimas', 'Img', 'Linkas']
+    csv_write = csv.DictWriter(file_writer, fieldnames, delimiter = ',')
+    csv_write.writeheader()
+    #csv_write.writerow
+    for i in range(len(medical_name)):
+        csv_write.writerow({"Pavadinimas": medical_name[i], "Img": medical_image_url[i], "Linkas": medical_page_url[i]})
